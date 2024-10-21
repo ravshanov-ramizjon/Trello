cb46709f2ef5df00402269760f539adde198240b
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2024-10-21 09:51:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// const cart = [{
+//     name: 'go home',
+//     title: 'Вступительное собрание'
+// },
+// {
+//     name: 'В процессе',
+//     title: 'go home'
+// },
+// {
+//     name: 'Готово'
+// }
+// ]
+// const box = document.querySelector('.box')
+// const end = document.querySelector('.end')
+// const btn = document.querySelector('.box-btn')
+
+// function reload(arr) {
+//     productsDiv.innerHTML = '';
+
+//     for (let item of arr) {
+//         const name = document.createElement('p');
+//         const title = document.createElement('span');
+        
+
+//         name.innerHTML = item.name;
+//         title.innerHTML = item.title,
+     
+
+//         end.classList.add('end');
+       
+
+//         box.append(name);
+//         end.append(span);
+//         countDiv.append(count);
+//     }
+// }
+// reload(cart)
